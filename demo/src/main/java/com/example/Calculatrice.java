d0782df72bf65f0c6ae0098e64c510bc631dd530
christinaRas <rasu.christina@gmail.com>
2025-11-21 00:37:09 +0400

    modify calculatrice and interface

--- demo/src/main/java/com/example/Calculatrice.java
+++ demo/src/main/java/com/example/Calculatrice.java
@@ -1,46 +1,51 @@
 package com.example;
 
 public class Calculatrice {
-    public float a;
-    public float b;
-    public float c;
+    public double a;
+    public double b;
+    public double c;
 
-    public float getA() {
-        return a;
-    }
-    public void setA(float a) {
+     public Calculatrice(double a, double b, double c) {
         this.a = a;
-    }
-    public float getB() {
-        return b;
-    }
-    public void setB(float b) {
         this.b = b;
-    }
-    public float getC() {
-        return c;
-    }
-    public void setC(float c) {
         this.c = c;
     }
 
-    public float secondDegre()
+    public double getA() {
+        return a;
+    }
+    public void setA(double a) {
+        this.a = a;
+    }
+    public double getB() {
+        return b;
+    }
+    public void setB(double b) {
+        this.b = b;
+    }
+    public double getC() {
+        return c;
+    }
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double[] CalculSecondDegre()
     {
-        int delta = b*b-(4*a*c);
+        double delta = b*b-(4*a*c);
 
         if(delta>0)
         {
-            float solution1 = (-b+Math.sqrt(delta))/(2.0*a);
-            float solution2 = (-b-Math.sqrt(delta))/(2.0*a);
-            return new float[] {delta, solution1, solution2};
+            double solution1 = (-b+Math.sqrt(delta))/(2.0*a);
+            double solution2 = (-b-Math.sqrt(delta))/(2.0*a);
+            return new double[] {delta, solution1, solution2};
         } else if(delta==0)
         {
-            float solution = -b/2.0*a;
-            return new float[] {delta,solution};
+            double solution = -b/2.0*a;
+            return new double[] {delta,solution};
         } else
         {
              return null;
         }
     }
-    
 }
